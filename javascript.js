ea2a65ef3bb7c84aff4d6a1570736b5bac5cb821
Fajar Sidik Priatna <fajar@suitmedia.com>
2020-09-30 15:16:02 +0700

    refactor code

--- javascript.js
+++ javascript.js
@@ -29,6 +29,11 @@ function changeNavigasi() {
     }
 }
 
+// let namaku = 'agus';
+// console.log(namaku);
+// let nama2 = [20, 'usep', 'arif'];
+// console.log(nama2);
+// console.log(nama2[1]);
 
 
 var slideIndex = 1;
@@ -45,17 +50,49 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slide-show");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+
+    if (slides !== undefined && slides.length != 0 && dots !== undefined || dots.length != 0) {
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(function () {
+            plusSlides(1);
+        }, 3000);
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(function () {
-        plusSlides(1);
-    }, 3000);
 }
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+includeHTML();
