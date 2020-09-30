10636a4fe6dd2936c33df4e200eb532b829f376a
cidut <mamangbagoy@gmail.com>
2020-09-30 13:09:04 +0700

    tes perubahan

--- javascript.js
+++ javascript.js
@@ -13,7 +13,33 @@ for (let i = 0; i < 4; i++) {
     node.innerHTML = node.innerHTML + isi_paragraph;
 }
 function myFunction() {
-    alert('clicked');
+    alert('Arsip percobaan javascript');
 }
-//comment testing/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide-show");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 1500);
+}
+
 //test codingan cidut/
